651657846b19c78e289626dd9df6ef7d118834dd
vflachaire <vflachaire@hotmail.com--list>
2022-11-14 23:03:08 +0100

    Ajout Text animé

--- 3-text-anim.js
+++ 3-text-anim.js
@@ -14,27 +14,60 @@ let data = [
     {
         pseudo: "Denis",
         age: 43,
-        technos: ["PHP", "React", "C-sharp"],
+        technos: ["PHP", "React", "C-Sharp"],
         admin: true,
     },
 ]
 
 const target = document.getElementById("target");
-let i = 0; let j = 0;
+const prenom = document.getElementById("prenom");
+let i = 0;// parcours le tableau technos 
+let j = 0;// parcours l'objet date
+let k = 0;//parcours les lettres
+let efface = false;
 
 const affiche = () => {
-    console.log(i + " - " + j);
+
+    // console.log("j = " + j);
+    // console.log("i = " + i);
+    // console.log("k = " + k);
+
     setTimeout ( () => { 
-        if (i < data[j].technos.length ) {
-            target.innerHTML += data[j].technos[i];
-            i++;
-            if (i == data[j].technos.length && j < data.length ) {
-                j++
-                i = 0;
+        if (efface == true) { //effacement des lettres
+            if (k > 0) {
+                target.lastChild.remove();
+                k--;
+            }
+            else { // k = 0, on stoppe l'effacement 
+                efface = false;
             }
             affiche();
         }
-    }, 1000)
+        else if (j < data.length) {
+            
+            if (k < data[j].technos[i].length ) {
+                target.innerHTML += "<span>" + data[j].technos[i][k] + "</span>";
+                prenom.innerHTML = data[j].pseudo + " ";
+                k++;
+                affiche();
+            }
+            else if (k == data[j].technos[i].length) { 
+                efface = true; // activation de l'effacement  
+                i++; // passage au mot suivant
+            
+                if (i == data[j].technos.length) { // passage au data suivant
+                    j++;
+                    i = 0;
+                }
+                if (j < data.length) { //laisse le dernier mot affiché
+                    setTimeout( () => { // pause à la fin d'affichage du mot
+                        affiche();
+                    },2000)
+                }    
+            }
+        }
+        
+    }, 100)
 }
 
 affiche();
